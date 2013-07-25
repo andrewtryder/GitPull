9148175ac11c0c719ffb85554a638aeaae382355
spline <reticulatingspline@github.com>
2013-07-25 07:43:32 -0400

    Fix a bug or two, docstring text, and add ability to use 'all' for all plugins.

--- plugin.py
+++ plugin.py
@@ -29,10 +29,10 @@ class GitPull(callbacks.Plugin):
     threaded = True
 
     def updateplugin(self, irc, msg, args, repodir):
-        """<plugin>
+        """[<plugin name>|<all>]
 
-        Do a git pull on a plugin directory.
-        Ex: upgradeplugin GitPull
+        Do a git pull on a plugin directory. Specify plugin name or all for each.
+        Ex: updateplugin GitPull OR updateplugin all
         """
 
         # make our "dict" of valid plugins/dictionaries
@@ -41,33 +41,40 @@ class GitPull(callbacks.Plugin):
         for plugindir in plugindirs:  # for each "plugin" dir.
             dirfiles = os.listdir(plugindir)  # get a list of files in each dir.
             for dirfile in dirfiles:  # iterate over each dir.
-                if os.path.isdir(os.path.join(plugindir, dirfile)):  # if they're directories.
+                if os.path.isdir(os.path.join(plugindir, dirfile, '.git')):  # if they're directories
                     dirdict[dirfile] = os.path.join(plugindir, dirfile)  # add into the dict.
         # validate input.
-        if repodir not in dirdict:
+        if repodir == "all":
+            workdirs = [f for (f, v) in dirdict.items()]  # copy all keys
+        elif repodir not in dirdict:
             irc.reply("ERROR: '{0}' is an invalid plugin. Valid: {1}".format(repodir, " | ".join(sorted(dirdict.keys()))))
             return
+        else:  # this means its not all but repodir is in dirdict.
+            workdirs = [repodir]  # we inject the single key into the list.
         # we're valid from here on.
-        cwd = dirdict[repodir]  # cwd has to be the dir (in the value)
-        command = ["git", "pull"]
-        # run the command.
-        pipe = subprocess.Popen(command, shell=False, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        (out, error) = pipe.communicate()
-        return_code = pipe.wait()
-        if return_code == 0:  # command worked.
-            outlines = out.split('\n')  # split on newlines into list.
-            if len(outlines) > 6:  # more than six lines.
-                output = " ".join(outlines.strip())  # make it all one line, separated by a space.
-                output = utils.str.normalizeWhitespace(output)  # have no doublespaces.
-                irc.reply("{0} :: {1}".format(repodir, output))
-            else:  # less than six lines.
-                for outline in outlines:  # output each line.
-                    if outline != '':  # don't output blank lines:
-                        irc.reply(outline.strip())
-        else:  # error.
-            error = error.replace('\n', ' ')  # replace newlines to spaces.
-            error = utils.str.normalizeWhitespace(error)  # make sure no doublespaces.
-            irc.reply("ERROR :: {0} :: {1}".format(repodir, error))
+        for workdir in workdirs:
+            # we're valid from here on.
+            cwd = dirdict[workdir]  # cwd has to be the dir (in the value)
+            command = ["git", "pull"]
+            # run the command.
+            pipe = subprocess.Popen(command, shell=False, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            (out, error) = pipe.communicate()
+            return_code = pipe.wait()
+            # check if command worked or not.
+            if return_code == 0:  # command worked.
+                outlines = out.split('\n')  # split on newlines into list.
+                if len(outlines) > 6:  # more than six lines.
+                    output = " ".join(outlines.strip())  # make it all one line, separated by a space.
+                    output = utils.str.normalizeWhitespace(output)  # have no doublespaces.
+                    irc.reply("{0} :: {1}".format(workdir, output))
+                else:  # less than six lines.
+                    for outline in outlines:  # output each line.
+                        if outline != '':  # don't output blank lines:
+                            irc.reply(outline.strip())
+            else:  # error.
+                error = error.replace('\n', ' ')  # replace newlines to spaces.
+                error = utils.str.normalizeWhitespace(error)  # make sure no doublespaces.
+                irc.reply("ERROR :: {0} :: {1}".format(repodir, error))
 
     updateplugin = wrap(updateplugin, [('checkCapability', 'owner'), ('somethingWithoutSpaces')])
 
